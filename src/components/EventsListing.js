d83a6e8250416426713feeac2f726fb876225433
Mullins, Benjamin <benm@umich.edu>
2022-03-06 14:01:02 -0500

    split into components

--- src/components/EventsListing.js
+++ src/components/EventsListing.js
@@ -1,24 +1,19 @@
 import './EventsListing.css';
+import EventInstance from "./EventInstance";
+import EventDateTime from "./EventDateTime";
+import EventInfo from "./EventInfo";
 
 const EventsListing = (props) => {
     return (<div className='events'>
             {props.events.map((eventInstance, index) => (
-                <div className='event' key={index}>
-                    <div className='event__datetime'>
-                        <div className='event__date'>
-                            {new Date(eventInstance.date_start).toLocaleDateString()}
-                        </div>
-                        <div className='event__start-end'>
-                            {eventInstance.time_start}-{eventInstance.time_end}
-                        </div>
-                    </div>
-                    <div className='event__info'>
-                        <h2>{eventInstance.event_title}</h2>
-                        <div className='event__description'>
-                            {eventInstance.description}
-                        </div>
-                    </div>
-                </div>
+                <EventInstance key={index}>
+                    <EventDateTime
+                        dateStart={eventInstance.date_start}
+                        timeStart={eventInstance.time_start}
+                        timeEnd={eventInstance.time_end}
+                    />
+                    <EventInfo title={eventInstance.event_title} description={eventInstance.description} />
+                </EventInstance>
             ))}
         </div>
     );
